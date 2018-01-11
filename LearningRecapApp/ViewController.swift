7ac4a54db172e3c4e65af3f728c64227f3d7036b
Vincenzo Pugliese <nokiotto80@gmail.com>
2018-01-11 14:56:47 +0100

    added  first sketch, TabBar , swap textboxes

--- LearningRecapApp/ViewController.swift
+++ LearningRecapApp/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ImageViewGeneral: UIImageView!
+    @IBOutlet weak var textBox1: UITextField!
+    @IBOutlet weak var textBox2: UITextField!
+    @IBOutlet weak var btnSwap: UIButton!
+    
+    
+    //swap text in 2 textboxes
+    @IBAction func btnSwap(_ sender: UIButton) {
+        
+        if (textBox1.text == ""  || textBox2.text == "") {
+         print("Text empty")
+        return //esce?
+        }
+        else {
+            var temp:  String
+            
+            
+            temp = textBox1.text!  //Variabile di scambio
+            textBox1.text = textBox2.text
+            textBox2.text = temp
 
+             }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btnSwap.layer.borderWidth = 2
+        btnSwap.layer.borderColor = UIColor.black.cgColor
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
